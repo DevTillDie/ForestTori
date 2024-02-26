136f320a311cd286e21dd0716f6352514145e1fc
Hyebin (Helia) <hyebin218@naver.com>
2024-02-26 21:12:49 +0900

    refactor: refactor plant model and DataManger

--- ForestTori/ForestTori/Source/Model/Plant.swift
+++ ForestTori/ForestTori/Source/Model/Plant.swift
@@ -10,6 +10,7 @@ import Foundation
 /// 식물 정보
 ///
 /// - characterName: 식물의 이름
+/// - characterImage: 식물의 이미지
 /// - characterDescription: 식물 소개
 /// - characterLevel: 식물의 레벨
 /// - mainQuest: 식물 메인 미션
@@ -18,6 +19,7 @@ import Foundation
 /// - totalDay: 식물의 성장 완료에 필요한 총 일수
 struct Plant {
     var characterName: String
+    var characterImage: String
     var characterDescription: String
     var characterLevel = 1
     var mainQuest: String
