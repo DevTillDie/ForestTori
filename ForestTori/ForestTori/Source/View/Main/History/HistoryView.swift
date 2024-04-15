b253e11ccf9a4831b0617411f62772820d0de8c7
Hyebin (Helia) <hyebin218@naver.com>
2024-04-15 22:42:26 +0900

    design: create HistoryView UI

---
+++ ForestTori/ForestTori/Source/View/Main/History/HistoryView.swift
@@ -0,0 +1,128 @@
+//
+//  HistoryView.swift
+//  ForestTori
+//
+//  Created by hyebin on 4/15/24.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    @State private var todayHistory = ""
+    @State private var showCustomPopup = false
+    
+    var body: some View {
+        VStack {
+            hisoryViewHeader
+            selectImageView
+            writeHistoryView
+        }
+    }
+}
+
+// MARK: - UI
+
+extension HistoryView {
+    private var hisoryViewHeader: some View {
+        HStack {
+            Button {
+                // TODO: Dismiss
+            } label: {
+                Image(systemName: "xmark")
+                    .foregroundStyle(.redPrimary)
+            }
+            
+            Spacer()
+            
+            Text("성장일지")
+                .font(.subtitleL)
+            
+            Spacer()
+            
+            Button {
+                // TODO: save Realm
+            } label: {
+                Text("완료")
+                    .font(.subtitleM)
+                    .foregroundStyle(.greenSecondary)
+            }
+        }
+        .padding(.horizontal, 20)
+    }
+    
+    private var selectImageView: some View {
+        RoundedRectangle(cornerRadius: 8)
+            .aspectRatio(4/3, contentMode: .fit)
+            .padding(.horizontal)
+            .foregroundStyle(.gray.opacity(0.2))
+            .overlay(
+                Image(systemName: "photo")
+                    .foregroundColor(.gray)
+            )
+            .onTapGesture {
+                withAnimation {
+                    showCustomPopup.toggle()
+                }
+            }
+            .overlay {
+                selectImagePopup
+                    .hidden(showCustomPopup)
+            }
+    }
+    
+    private var writeHistoryView: some View {
+        RoundedRectangle(cornerRadius: 5)
+            .fill(Color.gray10)
+            .stroke(.brownSecondary, lineWidth: 2)
+            .overlay(alignment: .top) {
+                TextField("오늘의 활동과 감정을 적어보세요", text: $todayHistory, axis: .vertical)
+                    .tint(.greenSecondary)
+                    .padding()
+            }
+            .padding()
+    }
+    
+    private var selectImagePopup: some View {
+        HStack {
+            Spacer(minLength: 56)
+            
+            VStack {
+                Button {
+                    // TODO: show camera
+                }label: {
+                    HStack {
+                        Text("사진 찍기")
+                        Spacer()
+                        Image(systemName: "camera")
+                            .font(.system(size: 22))
+                    }
+                }
+                
+                Divider()
+                
+                Button {
+                    // TODO: show imagePicker
+                }label: {
+                    HStack {
+                        Text("사진 선택")
+                        Spacer()
+                        Image(systemName: "photo")
+                            .font(.system(size: 22))
+                    }
+                }
+            }
+            .foregroundStyle(.black)
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 14)
+                    .fill(.white)
+            )
+            
+            Spacer(minLength: 56)
+        }
+    }
+}
+
+#Preview {
+    HistoryView()
+}
